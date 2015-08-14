0203b13389cfcfd6cebd8a822a7a902b77b7244e
BobMulder <bobmulder@outlook.com>
2015-08-14 19:51:17 +0200

    Notification cleanup

--- src/Controller/Admin/NotificationsController.php
+++ src/Controller/Admin/NotificationsController.php
@@ -26,15 +26,6 @@ class NotificationsController extends AppController
 
     public function index()
     {
-
-//        $this->Notifier->notify([
-//            'users' => 1,
-//            'vars' => [
-//                'title' => 'New user',
-//                'body' => 'A new user has been registered'
-//            ]
-//        ]);
-
         $this->set('notifications', $this->Notifier->allNotificationList());
 
         $this->Notifier->markAsRead();
